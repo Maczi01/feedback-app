6fd59b365f01a8501cf6609ba9cd3377175fdc00
matchi01 <mateusz.w.twardy@gmail.com>
2021-09-13 21:04:46 +0200

    Refactor code 2 (to check github actions)

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import {ThemeProvider} from "@material-ui/core/styles";
 import theme from "./theme/theme";
 import {CssBaseline} from "@material-ui/core";
 import Footer from "./components/Footer";
-import {Box, Typography, Button, TextField} from "@material-ui/core";
+import {Box} from "@material-ui/core";
 import ProductCard from "./components/ProductCard";
 
 const App = () => (
     <ThemeProvider theme={theme}>
         <CssBaseline/>
-        <Box bgcolor="#F2F4FF" >
+        <Box bgcolor="#F2F4FF">
             <Navbar/>
             <p>
                 Edit src/App.js and save to reload.
