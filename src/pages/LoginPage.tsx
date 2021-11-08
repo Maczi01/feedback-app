241ad5140202def013be7f860d9365416cf3a332
maczi01 <mateusz.w.twardy@gmail.com>
2021-11-08 22:53:36 +0000

    Changed package json file

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,89 @@
 import React from 'react';
-import { Button, TextField } from '@material-ui/core';
+import {
+  Box, Button, CardMedia, TextField, Theme,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ombre from '../assets/ombre.svg';
 
-const LoginPage: React.FC = () => (
-  <>
-    <form>
-      <TextField placeholder="title" />
-      <TextField placeholder="title" />
-      <TextField placeholder="title" />
+const useStyles = makeStyles((theme: Theme) => ({
+  form: {
+    padding: '20px',
+    margin: '50px',
+    backgroundColor: theme.palette.text.secondary,
+  },
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '20px',
+    margin: '10px 50px',
+    borderRadius: '10px',
+    width: '400px',
+  },
+  textField: {
+    margin: '20px',
+    padding: '10px',
+    border: '1px solid',
+    borderColor: theme.palette.text.primary,
+    borderRadius: '5px',
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  photo: {
+    width: '180px',
+    margin: '20px',
+    padding: '10px',
+  },
+  media: {
+    height: '100%',
+    // width: '100%'
+  },
+}));
 
-      <Button> Log in</Button>
-    </form>
-    <Button> Create account</Button>
-    <Button> Forgot password</Button>
-  </>
-);
+const LoginPage: React.FC = () => {
+  const classes = useStyles();
 
+  return (
+    <>
+      <form>
+        <Box className={classes.root}>
+          <TextField
+            label=""
+            id="filled-size-small"
+            defaultValue="Small"
+            variant="standard"
+            className={classes.textField}
+          />
+
+          <TextField
+            label=""
+            id="filled-size-small"
+            defaultValue="Small"
+            variant="standard"
+            className={classes.textField}
+          />
+
+          <TextField
+            label=""
+            id="filled-size-small"
+            defaultValue="Small"
+            variant="standard"
+            className={classes.textField}
+          />
+
+          <Button> Log in</Button>
+        </Box>
+      </form>
+      <Box className={classes.root}>
+        <Button color="secondary" variant="contained">
+          Create account
+        </Button>
+        <Button color="secondary" variant="contained">
+          Forgot password
+        </Button>
+      </Box>
+    </>
+  );
+};
 export default LoginPage;
